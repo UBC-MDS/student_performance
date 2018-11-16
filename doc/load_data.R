7b6c2fe8abcf691bf0dfefddc640a72dcb72356a
Yenan Zhang <yenanz@outlook.com>
2018-11-16 13:58:05 -0800

    set up repo structure

---
+++ doc/load_data.R
@@ -0,0 +1,5 @@
+
+
+# data downloaded from 
+# https://archive.ics.uci.edu/ml/datasets/Student+Performance?fbclid=IwAR2wU2nVAwPfCUkCBoE2_4ePwEwGEOmET4V_93u8ioieo7_3XMeqgGt2vzQ
+data <- read.csv("data/student-mat.csv", sep=";")
