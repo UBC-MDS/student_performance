c35ffee4ee3768938022eaaafffb0fc92ce87360
Zixin Zhang <zxzzhangg@gmail.com>
2018-11-22 20:32:24 -0800

    updated an analysis script

---
+++ src/analyze_data.R
@@ -0,0 +1,36 @@
+#! /usr/bin/env Rscript
+#
+# analyze_data.R
+#
+# This script reads in cleaned data register_student.csv and conduct a two-side t-test.
+#
+# This script takes input and output filepaths as parameters.
+#
+# Input filepath specifies location of the cleaned dataset and output filepath specifies
+# where to save the t-test result summary table(.csv file).
+
+# Usage: 
+# Rscript src/analyze_data.R data/registered_student.csv data/t_test.csv
+
+# Import libraries
+library(broom)
+
+# read in command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+input_file <- args[1]
+output_file <- args[2]
+
+# define main function
+main <- function(){
+  
+  # read in data
+  data <- read.csv(input_file)
+  
+  result <- tidy(t.test(G3 ~ romantic, data = data, var.equal = FALSE))
+  
+  write.csv(result, file = output_file)
+  
+}
+
+# call main function
+main()
